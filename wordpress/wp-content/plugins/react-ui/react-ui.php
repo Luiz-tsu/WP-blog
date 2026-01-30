47b1ee83f45bff44abfe308a79cf9d3837612e1c
Luiz Eduardo <40063261+Luiz-tsu@users.noreply.github.com>
2026-01-30 12:27:54 -0300

    feat(site): ajustes componente react

--- wordpress/wp-content/plugins/react-ui/react-ui.php
+++ wordpress/wp-content/plugins/react-ui/react-ui.php
@@ -1,27 +1,45 @@
 <?php
 /**
- * Plugin Name: React UI
- * Description: React components for WordPress frontend
- * Version: 1.0
+ * Plugin Name:       React UI
+ * Description:       React components for WordPress frontend
+ * Version:           1.0.0
+ * Requires at least: 6.0
+ * Requires PHP:      7.4
+ * Author:            Você
+ * Text Domain:       react-ui
  */
 
-if (!defined('ABSPATH')) exit;
+if ( ! defined( 'ABSPATH' ) ) {
+  exit;
+}
 
+/**
+ * Enfileira o bundle React no front-end
+ */
 add_action('wp_enqueue_scripts', function () {
   wp_enqueue_script(
-    'react-ui',
-    plugins_url('dist/assets/index.js', __FILE__),
+    'react-ui-frontend',
+    plugins_url('build/frontend.js', __FILE__),
     [],
     null,
     true
   );
-
-  wp_localize_script('react-ui', 'WP_DATA', [
-    'api'   => rest_url(),
-    'nonce' => wp_create_nonce('wp_rest')
-  ]);
 });
 
-add_shortcode('react_ui', function () {
-  return '<div id="react-ui"></div>';
+/**
+ * Shortcode para montar o React
+ * Uso:
+ * [react_ui type="text-type" texts="Hello|World"]
+ */
+add_shortcode('react_ui', function ($atts) {
+  $atts = shortcode_atts([
+    'type'  => '',
+    'texts' => ''
+  ], $atts);
+
+  return sprintf(
+    '<div class="react-ui" data-component="%s" data-texts="%s"></div>',
+    esc_attr($atts['type']),
+    esc_attr($atts['texts'])
+  );
 });
